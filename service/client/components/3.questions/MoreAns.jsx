380c83b25867f03328b3fac666ea05fd91d2a12f
aminehajamor <amineehajamorr@gmail.com>
2021-10-15 17:07:20 +0100

    style on click photo appear with style

---
+++ service/client/components/3.questions/MoreAns.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function MoreAns() {
+  return (
+
+    <div className="w-52 h-12  flex flex-row items-center border border-gray-600">
+      <button class="font-sans px-10 font-bold text-xs ml-4 text-gray-600">
+        MORE ANSWERS
+      </button>
+    </div>
+
+  )
+}
